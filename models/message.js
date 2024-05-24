f3b6efb414cbf6c5e90e6135f9bd986d896f2be0
AmrutiyaKirtan <kirtanamrutiya07@gmail.com>
2024-05-24 23:46:19 +0530

    refactoring

---
+++ models/message.js
@@ -0,0 +1,25 @@
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    conversation:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conversation',
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    timestamp:{
+        type: Date,
+        default: Date.now(),
+        required: false
+    }
+});
+
+module.exports = mongoose.model('message', messageSchema);
